36f5b1892c7a6b77940a8a0b32de63f1d67bb862
santos_raul <santos_raul@sicredi.com.br>
2023-04-09 20:16:59 -0300

    adicionando dependencia do rest assured e dev primeiro caso de teste

---
+++ src/test-e2e/java/funcional/CadastroClienteTest.java
@@ -0,0 +1,39 @@
+package funcional;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class CadastroClienteTest {
+
+    private static final String BASE_ENDPOINT = "http://localhost:8080/clientes";
+
+    /*
+    Dado nome, idade e email de um cliente
+    Quando uma requisição POST for efetuada
+    Então deverá retorar o status code 201
+     */
+
+    @Test
+    public void deveRetornarStatus201AoCadastrarCliente() {
+
+        given()
+                .body("""
+                        {
+                          "nome": "Nome Teste",
+                          "idade": 20,
+                          "email": "email@email.com"
+                        }""")
+                .contentType(ContentType.JSON)
+                .when()
+                .post(BASE_ENDPOINT)
+                .then()
+                .statusCode(201)
+                .body("nome", equalTo("Nome Teste"));
+
+    }
+
+}
