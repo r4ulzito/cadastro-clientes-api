86009835d3c59d0a3191568b85fbd35981f45d0f
santos_raul <santos_raul@sicredi.com.br>
2023-04-10 09:50:32 -0300

    base para testes

---
+++ src/test-e2e/java/funcional/CadastroClienteTest.java
@@ -0,0 +1,12 @@
+package funcional;
+
+public class CadastroClienteTest {
+
+    /*
+     <- Exemplo de BDD ->
+    Dado nome, idade e email válidos
+    Quando uma requisição POST for efetuada para "/clientes"
+    Então deverá retorar o status code 201
+     */
+
+}
