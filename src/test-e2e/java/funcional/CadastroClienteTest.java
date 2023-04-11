a105595824062320f545cb134273431a6f4d26e5
santos_raul <santos_raul@sicredi.com.br>
2023-04-11 11:30:03 -0300

    atualização teste base

--- src/test-e2e/java/funcional/CadastroClienteTest.java
+++ src/test-e2e/java/funcional/CadastroClienteTest.java
@@ -1,12 +1,20 @@
 package funcional;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CadastroClienteTest {
 
     /*
      <- Exemplo de BDD ->
     Dado nome, idade e email válidos
-    Quando uma requisição POST for efetuada para "/clientes"
+    Quando uma requisição POST for efetuada para o endpoint "/clientes"
     Então deverá retorar o status code 201
      */
+
+    private static final String BASE_ENDPOINT = "http://localhost:8080/clientes";
 
 }
