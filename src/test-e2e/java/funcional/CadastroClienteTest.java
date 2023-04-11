cd7d1dc32e40037fedf00c0321e90bb95de2a96b
santos_raul <santos_raul@sicredi.com.br>
2023-04-11 11:36:16 -0300

    remoção importaçoes inutilizadass

--- src/test-e2e/java/funcional/CadastroClienteTest.java
+++ src/test-e2e/java/funcional/CadastroClienteTest.java
@@ -1,11 +1,5 @@
 package funcional;
 
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
 public class CadastroClienteTest {
 
     /*
