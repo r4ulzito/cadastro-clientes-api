8b290a92ccba8cb7db4394a377a356030a34cff1
santos_raul <santos_raul@sicredi.com.br>
2023-04-12 11:01:42 -0300

    alteração teste base

--- src/test-e2e/java/funcional/CadastroClienteTest.java
+++ src/test-e2e/java/funcional/CadastroClienteTest.java
@@ -1,9 +1,21 @@
 package funcional;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class CadastroClienteTest {
 
     /*
-     <- Exemplo de BDD ->
+     <- Exemplo de BDD Cadastro de Cliente ->
     Dado nome, idade e email válidos
     Quando uma requisição POST for efetuada para o endpoint "/clientes"
     Então deverá retorar o status code 201
